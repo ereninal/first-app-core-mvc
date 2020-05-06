96ff6c51e5d63e60cc002e17e0d5888b8f9fb444
ereninal10@gmail.com <ereninal10@gmail.com>
2020-05-06 15:10:06 +0300

    the product model, amount column added

--- ShopApp.WebUI/Controllers/ProductController.cs
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -20,8 +20,12 @@ namespace ShopApp.WebUI.Controllers
         {
             var products = new List<Product>()
             {
-                new Product{Name = "Apple 8",Price=3000,Description="2018 date"},
-                new Product{Name = "Apple X",Price=5000,Description="2019 date"}
+                new Product{Name = "Apple 6",Price=3000,Description="2016 date",Amount=5},
+                new Product{Name = "Apple 6S",Price=4000,Description="2016 date",Amount=15},
+                new Product{Name = "Apple 7",Price=5000,Description="2017 date",Amount=25},
+                new Product{Name = "Apple 7 PLUS",Price=6000,Description="2017 date",Amount=4},
+                new Product{Name = "Apple 8",Price=7000,Description="2018 date",Amount=2},
+                new Product{Name = "Apple 8 PLUS",Price=8000,Description="2018 date",Amount=30},
             };
             var category = new Category{Name="Telefon",Desciprtion="IOS"};
             var models = new ProductViewModel{
--- ShopApp.WebUI/Models/Product.cs
+++ ShopApp.WebUI/Models/Product.cs
@@ -5,6 +5,8 @@ namespace ShopApp.WebUI.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+
+        public int Amount { get; set; }
         
     }
 }
